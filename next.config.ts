83b0bb61e7f6e3105ef79c7d573edf575566b10e
timo kerola <timo.kerola777@gmail.com>
2026-06-08 00:22:04 +0300

    file path fix

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/color-code",
+  assetPrefix: "/color-code",
 };
 
 export default nextConfig;
